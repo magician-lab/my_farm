b4202f31ba262d30fd2a610145b6c77d06b0043b
magician-lab <magicdevelopers9@gmail.com>
2026-06-23 09:26:03 +0300

    recover stale render

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'supersecretkey123'
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300
+}
 
 db.init_app(app)
 
