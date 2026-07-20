528978141b471e432691ebe9d4cb76c29438521c
magician-lab <magicdevelopers9@gmail.com>
2026-07-20 15:54:05 +0300

    postress environment

--- app.py
+++ app.py
@@ -21,10 +21,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.gotev(
-     "DATABASE_URL",
-    "sqlite:///database.db"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'supersecretkey123'
